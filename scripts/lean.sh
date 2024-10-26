39fce7e78ac16fb03d0ad254857ed4ee2a3116e2
ahapuWang <55306019+ahapu@users.noreply.github.com>
2024-10-26 12:53:45 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -8,12 +8,6 @@
 #=================================================
 
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
-# alist
-git clone https://github.com/sbwml/luci-app-alist package/alist
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-
-# Clone community packages
 mkdir package/community
 pushd package/community
 
@@ -23,6 +17,9 @@ rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
 rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
+# apppppppp
+git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
+
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
 
@@ -31,21 +28,8 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
-# Add luci-app-unblockneteasemusic
-rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
-
-# Add luci-proto-minieap
-git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
-
-# Add luci-app-onliner (need luci-app-nlbwmon)
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-
 # Add OpenClash
 git clone --depth=1 https://github.com/vernesong/OpenClash
-
-# Add luci-app-poweroff
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
 
 # Add luci-theme
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
@@ -56,22 +40,17 @@ rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 cp -f $GITHUB_WORKSPACE/data/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 git clone https://github.com/DHDAXCW/theme
 
-# Add subconverter
-git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+# Add luci-app-lucky
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky
 
 # alist
-git clone --depth=1 https://github.com/sbwml/openwrt-alist
+git clone -b lua --depth=1 https://github.com/sbwml/luci-app-alist
 
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
 
-# Add luci-aliyundrive-webdav
-rm -rf ../../customfeeds/luci/applications/luci-app-aliyundrive-webdav
-rm -rf ../../customfeeds/packages/multimedia/aliyundrive-webdav
-git clone --depth=1 https://github.com/messense/aliyundrive-webdav
-mkdir -p linkease
 popd
-
+ 
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
@@ -81,37 +60,16 @@ export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
 sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
 popd
 
-# Add ddnsto & linkease
-pushd package/community/linkease
-git clone --depth=1 https://github.com/linkease/nas-packages-luci
-git clone --depth=1 https://github.com/linkease/nas-packages
-cd nas-packages-luci
-rm -rf luci-app-istorex luci-app-quickstart luci-app-linkease luci-app-unishare && cd ../
-cd nas-packages/network/services
-rm -rf linkease quickstart unishare webdav2 && cd ../../ && rm -rf multimedia/ffmpeg-remux && cd ../
-popd
-
 rm -rf nas-packages-luci/luci/luci-app-istorex
-
-# Add ServerChan
-git clone --depth=1 https://github.com/tty228/luci-app-serverchan
-
-# Add luci-app-ikoolproxy (godproxy)
-git clone --depth=1 https://github.com/iwrt/luci-app-ikoolproxy.git
-rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf package/feeds/packages/libmbim
+rm -rf package/feeds/packages/lame
+rm -rf package/feeds/packages/apk
+rm -rf package/feeds/packages/adguardhome
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.23.1/g' package/base-files/files/bin/config_generate
-
-# 风扇脚本
-wget -P target/linux/rockchip/armv8/base-files/etc/init.d/ https://github.com/friendlyarm/friendlywrt/blob/master-v23.05/target/linux/rockchip/armv8/base-files/etc/init.d/fa-fancontrol
-wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v23.05/target/linux/rockchip/armv8/base-files/usr/bin/cputemp.sh
-wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v23.05/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
-wget -P target/linux/rockchip/armv8/base-files/usr/bin/ https://github.com/friendlyarm/friendlywrt/blob/master-v23.05/target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh
-chmod u+x target/linux/rockchip/armv8/base-files/etc/init.d/fa-fancontrol
-chmod u+x target/linux/rockchip/armv8/base-files/usr/bin/cputemp.sh
-chmod u+x target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
-chmod u+x target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
